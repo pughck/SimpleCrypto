612ccd9da99e6ce7064da43f988112942bbac3a6
Larry Gates <gateslm@rose-hulman.edu>
2016-01-17 22:41:09 -0500

    Added title
    Made title multiple colors

--- app/src/main/java/edu/rosehulman/pughck/simplecrypto/MainActivity.java
+++ app/src/main/java/edu/rosehulman/pughck/simplecrypto/MainActivity.java
@@ -1,7 +1,10 @@
 package edu.rosehulman.pughck.simplecrypto;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.abs_layout);
+        TextView abTv = (TextView) findViewById(R.id.action_bar_text);
+        abTv.setText(Html.fromHtml(getString(R.string.title_bar)));
     }
 }
