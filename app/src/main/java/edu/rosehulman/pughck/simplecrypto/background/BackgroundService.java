cd44a92f54e04ff4c5348b05deda21145a1a2460
Larry Gates <gateslm@rose-hulman.edu>
2016-02-13 11:15:35 -0500

    Got part of it working
    Problems with closing app completely and loosing firebase connection.
    However, it send push notifications.

--- app/src/main/java/edu/rosehulman/pughck/simplecrypto/background/BackgroundService.java
+++ app/src/main/java/edu/rosehulman/pughck/simplecrypto/background/BackgroundService.java
@@ -1,9 +1,14 @@
 package edu.rosehulman.pughck.simplecrypto.background;
 
+import android.annotation.TargetApi;
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.content.Context;
+import android.os.Build;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -13,6 +18,8 @@ import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
 
+import edu.rosehulman.pughck.simplecrypto.MainActivity;
+import edu.rosehulman.pughck.simplecrypto.R;
 import edu.rosehulman.pughck.simplecrypto.utilities.Constants;
 
 /**
@@ -47,6 +54,7 @@ public class BackgroundService extends Service {
         this.listener = new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.d(Constants.background_service_message, firebase.getAuth().getUid()+ "");
                 notifyUser(dataSnapshot);
             }
 
@@ -63,5 +71,16 @@ public class BackgroundService extends Service {
     private void notifyUser(DataSnapshot dataSnapshot) {
         Log.d(Constants.background_service_message, "Got a snapshot");
         Log.d(Constants.background_service_message, dataSnapshot.toString());
+        Context context = getApplicationContext();
+        Intent notificationIntent = new Intent(context, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context
+                .NOTIFICATION_SERVICE);
+        int icon = R.drawable.default_profile;
+        Notification n = new Notification.Builder(this).setContentTitle("My message")
+                .setContentText("Subject").setSmallIcon(R.drawable.default_profile)
+                .setContentIntent(contentIntent).setAutoCancel(true).build();
+        n.flags |= Notification.FLAG_AUTO_CANCEL;
+        notificationManager.notify(0,n);
     }
 }
